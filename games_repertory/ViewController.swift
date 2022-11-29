d76c09ffaf972c84e4e2c0a6c202293bd307ed6f
umutulasdemir <umutulasdemir.uud@gmail.com>
2022-11-29 14:55:27 +0300

    Initial Commit

---
+++ games_repertory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  games_repertory
+//
+//  Created by Umut Ulaş Demir on 29.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ games_repertoryUITests/games_repertoryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  games_repertoryUITestsLaunchTests.swift
+//  games_repertoryUITests
+//
+//  Created by Umut Ulaş Demir on 29.11.2022.
+//
+
+import XCTest
+
+final class games_repertoryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
