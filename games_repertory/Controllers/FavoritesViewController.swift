b4fc663574b02ae904e9470cb2d4aeaaff9c2e00
Bora <boraeroglu238@hotmail.com>
2022-12-13 01:06:40 +0300

    Bug Fix - Fix bug where sliding a favorite game to the left to remove from favorites would not in fact do so

--- games_repertory/Controllers/FavoritesViewController.swift
+++ games_repertory/Controllers/FavoritesViewController.swift
@@ -23,13 +23,16 @@ class FavoritesViewController: UIViewController, UITabBarDelegate {
         //getFavorites(isFavList: favoriteGamesList, games: targetgGames)
         LoadGamesData()
     }
-    private func LoadGamesData() {
+    
+    
+    private func LoadGamesData() { // Called at the beginning to do an API call and fill targetGames
         gameViewModel.fetchGamesData{ [weak self] in
                 self?.tableView.dataSource = self
                 self?.tableView.reloadData()
         }
     }
     
+    // Pull favoriteGamesList and targetGames from tab bar, change local targetGames to only include favorites
     override func viewWillAppear(_ animated: Bool) {
         let tabbar2 = tabBarController as! BaseUITabBarController?
         favoriteGamesList = tabbar2?.favoriteGamesList
@@ -38,11 +41,14 @@ class FavoritesViewController: UIViewController, UITabBarDelegate {
         loadImages(games: gameViewModel.getGames())
 
     }
+    
+    // Pass the most relevant favoriteGamesList to the tab bar
     override func viewWillDisappear(_ animated: Bool) {
         let tabbar = self.tabBarController as! BaseUITabBarController?
         tabbar?.favoriteGamesList = self.favoriteGamesList
     }
     
+    // Change local targetGames to only include favorites
     func getFavorites(isFavList: [Bool]!,games: [Game]!){
         gameViewModel.clearData()
         var p = 0
@@ -52,8 +58,10 @@ class FavoritesViewController: UIViewController, UITabBarDelegate {
             }
             p+=1
         }
-        tableView.reloadData()
+        tableView.reloadData() // Load the table with current list
     }
+    
+    // Call loadImage() for each game in given list
     func loadImages(games: [Game]!){
         print("ALO:", games.count)
         var i = 0
@@ -62,6 +70,8 @@ class FavoritesViewController: UIViewController, UITabBarDelegate {
             i+=1
         }
     }
+    
+    // If the given string can be converted to a URL, call getImageDataFrom() to do an API call. Else, fill with placeholder.
     func loadImage(urlS: String!, index: Int){
         guard let posterString = urlS else {return}
         let urlString = posterString
@@ -71,6 +81,8 @@ class FavoritesViewController: UIViewController, UITabBarDelegate {
         }
         getImageDataFrom(url: posterImageURL, index: index)
     }
+    
+    // Get the game image via an API call and put it into the given index of the image list
     private func getImageDataFrom(url: URL, index: Int) {
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             // Handle Error
@@ -97,6 +109,8 @@ class FavoritesViewController: UIViewController, UITabBarDelegate {
 
 
 extension FavoritesViewController: UITableViewDataSource,  UITableViewDelegate{
+    
+    // Initialize the table depending on the amount of games in the list
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let count = gameViewModel.numberOfRowsInSection(section: section)
         
@@ -114,23 +128,26 @@ extension FavoritesViewController: UITableViewDataSource,  UITableViewDelegate{
                     return max(count,1)
                 }
 
-        
+    // Called for each game in the list
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell : GameCell
-        if gameViewModel.getCount() != 0{
-            cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! GameCell
-            let game = gameViewModel.cellForRowAt(indexPath: indexPath)
-            cell.setCellWithValuesOf(game, image: images![indexPath.row])
+        var cell : GameCell // Declare the cell
+        if gameViewModel.getCount() != 0{ // If there are games to display
+            cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! GameCell // Initialize cell
+            let game = gameViewModel.cellForRowAt(indexPath: indexPath) // Get the game data from the list
+            cell.setCellWithValuesOf(game, image: images![indexPath.row]) // Fill in the cell with the game data
             self.tableView.separatorStyle = UITableViewCell.SeparatorStyle.singleLine
         }
         else {
-            cell = tableView.dequeueReusableCell(withIdentifier: "noCell2", for: indexPath) as! GameCell
-            self.tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+            cell = tableView.dequeueReusableCell(withIdentifier: "noCell2", for: indexPath) as! GameCell // Initialize noCell
+            self.tableView.separatorStyle = UITableViewCell.SeparatorStyle.none // Remove seperator for a better look
         }
         return cell
     }
     
+    // Called when the client swipes a game to the left
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        
         let delete = UIContextualAction(style: .destructive, title: "Delete") { (contextualAction, view, actionPerformed: @escaping (Bool) -> ()) in
             let alert = UIAlertController(title: "Remove from favorites?", message: "Are you sure you want to remove this game from favorites?", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (alertAction) in
@@ -138,8 +155,14 @@ extension FavoritesViewController: UITableViewDataSource,  UITableViewDelegate{
             }))
             alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { (alertAction) in
                 tableView.beginUpdates()
+                let deletedId = self.gameViewModel.getGames()[indexPath.row].id
                 self.gameViewModel.removeGame(index: indexPath.row)
-                self.favoriteGamesList![indexPath.row] = false
+                for (i, game) in self.targetgGames.enumerated(){
+                        if game.id == deletedId{
+                            self.favoriteGamesList![i] = false
+                            break
+                        }
+                    }
                 let tabbar = self.tabBarController as! BaseUITabBarController?
                 tabbar?.favoriteGamesList = self.favoriteGamesList
                 if(self.gameViewModel.getCount() != 0){
